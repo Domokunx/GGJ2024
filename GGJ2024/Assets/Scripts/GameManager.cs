9bbddc680fa8a6062f52f7e719a69e07cfeb0890
Domokunx <cliffkoes@gmail.com>
2024-01-27 19:11:50 +0800

    BrainGame Randomized

--- GGJ2024/Assets/Scripts/GameManager.cs
+++ GGJ2024/Assets/Scripts/GameManager.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private static int nextDate = 2;
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
     }
 
+    public static IEnumerator MoveToNextDate()
+    {
+        // Play transition for 5s
+        yield return new WaitForSeconds(4f);
+
+        // Load next date
+        SceneManager.LoadScene(nextDate++);
+        
+    }
     public static IEnumerator BackToOutfitSelector(GameObject transition)
     {
         // Play transition for 5s
@@ -19,6 +29,6 @@ public class GameManager : MonoBehaviour
 
         // Move back to outfitSelector
         
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("OutfitSelector");
     }
 }
