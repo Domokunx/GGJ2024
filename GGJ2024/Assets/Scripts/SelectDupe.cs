8d8510af3e20c3e543bad643b0b2f2b83eb6fd36
23-Matsuo <it222225@trident.ac.jp>
2024-01-27 17:31:54 +0900

    Wald Game Prototype done

--- GGJ2024/Assets/Scripts/SelectDupe.cs
+++ GGJ2024/Assets/Scripts/SelectDupe.cs
@@ -8,53 +8,84 @@ public class SelectDupe : MonoBehaviour
     [SerializeField] private GameObject finishText;
     [SerializeField] private int correctNum = 3;
 
+    [Header("Zoom Setting")]
+    [SerializeField] private float zoomInSize = 3f;
+    [SerializeField] private float zoomOutSize = 5f;
+    [SerializeField] private float zoomTime = 0.1f;
+
     private GameObject hitObject;
     private Spawner spawner;
 
-    private bool mistake;
+    private bool isCorrect;
+    private bool isMistake;
     private bool finished;
 
     private int correctCount;
 
+    private Camera camera;
+
+    private bool zoomed;
+
+    private float timer;
+
+    enum State
+    {
+        Idle,
+        ZoomIn,
+        ZoomOut,
+    }
+
+    private State state;
+
     // Start is called before the first frame update
     void Start()
     {
-        mistake = false;
+        isMistake = false;
+        isCorrect = false;
         finished = false;
         hitObject = null;
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         finishText.SetActive(false);
+        camera = Camera.main;
+        zoomed = false;
+
+        state = State.Idle;
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (finished)
+        switch (state)
         {
-            finishText.SetActive(true);
-            return;
+            case State.Idle:
+                {
+
+                    if (finished)
+                    {
+                        finishText.SetActive(true);
+                        return;
+                    }
+
+                    ClickDupes();
+
+
+                    break;
+                }
+
+            case State.ZoomIn:
+                {
+                    ZoomIn(hitObject);
+                    //StartCoroutine(ZoomIn(hitObject));
+                    break;
+                }
+            case State.ZoomOut:
+                {
+                    ZoomOut();
+                    //StartCoroutine(ZoomOut());
+                    break;
+                }
         }
-
-        ClickDupes();
-
-
-        //if(mistake)
-        //{
-        //    StartCoroutine(FadeOut(hitObject));
-        //}
-    }
-
-    public IEnumerator FadeOut(GameObject gameObject)
-    {
-        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-
-        spriteRenderer.color = new Color(
-            spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, spriteRenderer.color.a - 0.05f);
-
-        yield return new WaitForSeconds(2f);
-
-        mistake = false;
-        hitObject = null;
     }
 
     private void ClickDupes()
@@ -72,24 +103,109 @@ public class SelectDupe : MonoBehaviour
 
             if (hitObject.tag != "Date")
             {
-                mistake = true;
+                isMistake = true;
                 Destroy(hitObject);
             }
             else
             {
+                isCorrect = true;
                 correctCount++;
-                if (correctCount == correctNum)
-                {
-                    finished = true;
-                    return;
-                }
-                spawner.SpawnDupes();
 
-                // change date position
-                Vector2 randomPosition = new Vector2(Random.Range(-9f, 9f), Random.Range(-5f, 0f));
-                hitObject.transform.position = new Vector3(randomPosition.x, randomPosition.y, 0);
-
+                // change state
+                state = State.ZoomIn;
             }
+        }
+    }
+
+
+    //public IEnumerator FadeOut(GameObject gameObject)
+    //{
+    //    SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+    //    spriteRenderer.color = new Color(
+    //        spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, spriteRenderer.color.a - 0.05f);
+
+    //    yield return new WaitForSeconds(2f);
+
+    //    isMistake = false;
+    //    hitObject = null;
+    //}
+    
+    private void ZoomIn(GameObject gameObject)
+    {
+        Vector3 targetPos = gameObject.transform.position;
+        targetPos.z = camera.transform.position.z;
+        // camera zoom
+        camera.transform.position = Vector3.Lerp(camera.transform.position, targetPos, zoomTime);
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoomInSize, zoomTime);
+
+        timer += Time.deltaTime;
+
+        if (timer > 2.0f)
+        {
+            Destroy(hitObject);
+
+            if (correctCount == correctNum)
+            {
+                finished = true;
+            }
+            else
+            {
+                spawner.SpawnDupes();
+                spawner.SpawnChicken();
+            }
+
+            state = State.ZoomOut;
+            timer = 0f;
+
+        }
+    }
+
+    //public IEnumerator ZoomIn(GameObject gameObject)
+    //{
+    //    Vector3 targetPos = gameObject.transform.position;
+    //    targetPos.z = camera.transform.position.z;
+    //    // camera zoom
+    //    camera.transform.position = Vector3.Lerp(camera.transform.position, targetPos, zoomTime);
+    //    camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoomInSize, zoomTime);
+
+    //    yield return new WaitForSeconds(2f);
+
+    //    // change chicken position
+    //    Destroy(hitObject);
+
+    //    if(state == State.ZoomIn)
+    //    {
+    //        spawner.SpawnDupes();
+    //        spawner.SpawnChicken();
+    //    }
+
+    //    state = State.ZoomOut;
+
+    //    hitObject = null;
+    //}
+
+    //public IEnumerator ZoomOut()
+    //{
+    //    camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(0f, 0f, camera.transform.position.z), zoomTime);
+    //    camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoomOutSize, zoomTime);
+
+    //    yield return new WaitForSeconds(0.5f);
+
+    //    state = State.Idle;
+    //}
+    private void ZoomOut()
+    {
+        camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(0f, 0f, camera.transform.position.z), zoomTime);
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoomOutSize, zoomTime);
+
+        timer += Time.deltaTime;
+
+        if (timer > 0.5f)
+        {
+            state = State.Idle;
+            timer = 0f;
+
         }
     }
 }
