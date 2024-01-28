7ad179b0d1e7446ab8a3fc957d67378891032eae
zyuqingz <zzyuqingzz@gmail.com>
2024-01-28 15:32:46 +0800

    clean up for build

--- GGJ2024/Assets/Scripts/OutfitScoreManager.cs
+++ GGJ2024/Assets/Scripts/OutfitScoreManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class OutfitScoreManager : MonoBehaviour
 {
