9179c2f7e5ef1a9c187ffa4100351c0434411cc9
zyuqingz <zzyuqingzz@gmail.com>
2024-01-28 09:29:23 +0800

    TypingGame_Transition done

--- GGJ2024/Assets/Scripts/GameTimer.cs
+++ GGJ2024/Assets/Scripts/GameTimer.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
+    [SerializeField] private GameObject transitionScreen;
     [SerializeField] private int timeLimit = 60;
 
     private Image timerImage;
-    private float spendingTime = 0f;
+    public float spendingTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
         timerImage = GetComponent<Image>();
+        transitionScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +24,10 @@ public class GameTimer : MonoBehaviour
         float timer = spendingTime / timeLimit;
 
         timerImage.fillAmount = timer;
+
+        if (spendingTime > timeLimit)
+        {
+            StartCoroutine(GameManager.BackToOutfitSelector(transitionScreen));
+        }
     }
 }
